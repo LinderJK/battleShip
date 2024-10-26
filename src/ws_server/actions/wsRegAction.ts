ce2fd05f0998c1ed5a8a53bc541a3f207b8d7fa3
LinderJK <slepchenkovia@gmail.com>
2024-10-26 18:58:25 +0300

    feat: add reg actions

---
+++ src/ws_server/actions/wsRegAction.ts
@@ -0,0 +1,43 @@
+import { savePlayer } from '../models/usersModel'
+import { IRegRequest, wsMessages } from '../types/wsTypes'
+
+export const wsRegAction = (
+    message: wsMessages,
+    callback: (data: string) => void
+) => {
+    let data = message.data
+
+    if (typeof data === 'string') {
+        data = JSON.parse(data)
+    }
+
+    const player = savePlayer(data as IRegRequest)
+
+    if (!player) {
+        callback(
+            JSON.stringify({
+                type: 'reg',
+                data: JSON.stringify({
+                    name: '',
+                    index: 0,
+                    error: true,
+                    errorText: `User already exists`,
+                }),
+                id: 0,
+            })
+        )
+    } else {
+        callback(
+            JSON.stringify({
+                type: 'reg',
+                data: JSON.stringify({
+                    name: player.name,
+                    index: player.index,
+                    error: false,
+                    errorText: '',
+                }),
+                id: 0,
+            })
+        )
+    }
+}
