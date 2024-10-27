e82108a559224e55427605aa0c8353a8b6d14180
LinderJK <slepchenkovia@gmail.com>
2024-10-27 18:24:30 +0300

    feat: add types

--- src/ws_server/types/wsTypes.ts
+++ src/ws_server/types/wsTypes.ts
@@ -15,10 +15,46 @@ export interface IUpdateWinners {
     wins: number
 }
 
-export type wsPayload = IRegRequest | IRegResponse
+type TUpdateWinners = Array<IUpdateWinners>
+
+export interface ICreateRoomRequest {
+    type: 'create_room'
+    data: string
+    id: number
+}
+
+export interface IAddUserToRoomRequest {
+    indexRoom: number | string
+}
+
+export interface IUpdateRoomResponse {
+    type: 'update_room'
+    data: Array<{
+        roomId: number | string
+        roomUsers: Array<{
+            name: string
+            index: number | string
+        }>
+    }>
+    id: number
+}
+
+export interface ICreateGameResponse {
+    idGame: number | string
+    idPlayer: number | string
+}
+
+export type wsPayload =
+    | IRegRequest
+    | IRegResponse
+    | TUpdateWinners
+    | ICreateRoomRequest
+    | IAddUserToRoomRequest
+    | ICreateGameResponse
+    | IUpdateRoomResponse
 
 export interface wsMessages {
     type: string
-    data: wsPayload
+    data: wsPayload | string
     id: number
 }
